a8326e38eb1df13fe492946c3d717321896b4e88
fei.lu <fei.lu@agfa.com>
2019-01-21 18:24:35 +0800

    optimization for code: reducer function

--- src/app/+state/app.reducers.ts
+++ src/app/+state/app.reducers.ts
@@ -1,5 +1,5 @@
 import { ActionReducer, Action, ActionReducerMap } from '@ngrx/store';
-import { ActionTypes, AliensGot, AlienDeleted, AlienUpdated } from './app.actions';
+import { ActionTypes, AliensGot, AlienDeleted, AlienUpdated, AlienAction } from './app.actions';
 import { Alien } from '../+core';
 
 
@@ -16,20 +16,24 @@ export const initialState: AppState = {
   loading: false
 };
 
-export const appReducer: ActionReducer<AppState> = (state: AppState = initialState, action: Action): AppState => {
+export const appReducer: ActionReducer<AppState> = (state: AppState = initialState, action: AlienAction): AppState => {
   switch (action.type) {
     case ActionTypes.ALIENS_GOT:
-      const nState = Object.assign({}, state, { aliens: (<AliensGot>action).payload, loading: false });
-      return nState;
+      return { ...state, aliens: action.payload, loading: false };
+
     case ActionTypes.ALIEN_DELETED:
-      return Object.assign({}, state, { aliens: state.aliens.filter(item => (<AlienDeleted>action).payload !== item.id) });
+      return { ...state, aliens: state.aliens.filter(item => action.payload !== item.id) };
+
     case ActionTypes.ALIEN_NEWED:
-      return Object.assign({}, state, { aliens: [...state.aliens, (<AliensGot>action).payload] });
+      return { ...state, aliens: [...state.aliens, action.payload] };
+
     case ActionTypes.ALIEN_UPDATED:
-      return Object.assign({}, state, { aliens: [state.aliens.filter(item => (<AlienUpdated>action).payload.id !== item.id),
-        (<AliensGot>action).payload] });
+      return { ...state, aliens: [...state.aliens.filter(item => (<AlienUpdated>action).payload.id !== item.id),
+        action.payload] };
+
     case ActionTypes.GET_ALIENS:
       return { ...state, loading: true };
+
     default:
       return state;
   }
